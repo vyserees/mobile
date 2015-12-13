983d913902f390017d1ee2507a020bd762afab70
Vladimir Veljkovic <vyserees@gmail.com>
2015-12-13 13:50:30 +0100

    Magnifier

--- app/views/kupovina/proizvod/index.php
+++ app/views/kupovina/proizvod/index.php
@@ -9,8 +9,10 @@ mvc_header();
         <div class="single">
             <div class="col-lg-5">
                 <div class="single-img-layer">
-                    <div class="single-img-in">
-                        <img src="" alt="Slika proizvoda" class="img-responsive"/>
+                    <div class="single-img-in magnify">
+                        <div class="large"></div>
+                        <img src="" alt="Slika proizvoda" class="img-responsive small"/>
+                        
                     </div>
                 </div>
                 <div class="single-thumbs">
@@ -28,7 +30,7 @@ mvc_header();
                     <article>
                         <em class="single-opis"></em>
                     </article>
-                    <div class="add-to-cart" data-itm="">
+                    <div class="add-to-cart" data-itm="" style="text-align: left;">
                         <button class="addtocart" data-prid="">DODAJTE U KORPU</button>
                     </div>
                 </div>
@@ -43,6 +45,14 @@ mvc_header();
 </div>
 <script>
 $(document).ready(function(){
+    /*----events----*/
+    $(document).on('click','.single-thumbnails',function(){
+        var url = $(this).attr('src');
+        $('.small').attr('src',url);
+        $('.large').css('background','url("'+url+'") no-repeat');
+    });
+    /*----events----*/
+    
     $('.add-to-cart').attr('data-itm',localStorage.proizvod);
     $('.addtocart').attr('data-prid',localStorage.proizvod);
     $('.add-to-cart').each(function(){
@@ -68,12 +78,13 @@ $(document).ready(function(){
         data:{prid:localStorage.proizvod},
         success:function(data){
             var sl = data.slike;
-            $('.single-img-in img').attr('src','/assets/images/products/'+sl[0].sli_url);
+            $('.small').attr('src','/assets/images/products/'+sl[0].sli_url);
+            $('.large').css('background','url("/assets/images/products/'+sl[0].sli_url+'") no-repeat');
             var l = sl.length;
             $('.single-thumbs').append('<div class="owl-carousel" id="single-carousel">');
             for(var i=0;i<l;i++){
                 $('#single-carousel').append(
-                    '<div><img src="/assets/images/products/'+sl[i].sli_url+'" alt="Male slike" class="single-thumbnails img-responsive" /></div>'    
+                    '<div style="margin-right:10px;"><img src="/assets/images/products/'+sl[i].sli_url+'" alt="Male slike" class="single-thumbnails img-responsive" /></div>'    
                     );
             }
             $('.single-thumbs').append('<div>');
@@ -94,6 +105,74 @@ $(document).ready(function(){
             $('.single-opis').html(pr[0].pro_opis);
         }    
     });
+    
+    /*------magnifier------*/
+    
+    var native_width = 0;
+	var native_height = 0;
+
+	//Now the mousemove function
+	$(".magnify").mousemove(function(e){
+		//When the user hovers on the image, the script will first calculate
+		//the native dimensions if they don't exist. Only after the native dimensions
+		//are available, the script will show the zoomed version.
+		if(!native_width && !native_height)
+		{
+			//This will create a new image object with the same image as that in .small
+			//We cannot directly get the dimensions from .small because of the 
+			//width specified to 200px in the html. To get the actual dimensions we have
+			//created this image object.
+			var image_object = new Image();
+			image_object.src = $(".small").attr("src");
+			
+			//This code is wrapped in the .load function which is important.
+			//width and height of the object would return 0 if accessed before 
+			//the image gets loaded.
+			native_width = image_object.width;
+			native_height = image_object.height;
+		}
+		else
+		{
+			//x/y coordinates of the mouse
+			//This is the position of .magnify with respect to the document.
+			var magnify_offset = $(this).offset();
+			//We will deduct the positions of .magnify from the mouse positions with
+			//respect to the document to get the mouse positions with respect to the 
+			//container(.magnify)
+			var mx = e.pageX - magnify_offset.left;
+			var my = e.pageY - magnify_offset.top;
+			
+			//Finally the code to fade out the glass if the mouse is outside the container
+			if(mx < $(this).width() && my < $(this).height() && mx > 0 && my > 0)
+			{
+				$(".large").fadeIn(100);
+			}
+			else
+			{
+				$(".large").fadeOut(100);
+			}
+			if($(".large").is(":visible"))
+			{
+				//The background position of .large will be changed according to the position
+				//of the mouse over the .small image. So we will get the ratio of the pixel
+				//under the mouse pointer with respect to the image and use that to position the 
+				//large image inside the magnifying glass
+				var rx = Math.round(mx/$(".small").width()*native_width - $(".large").width()/2)*-1;
+				var ry = Math.round(my/$(".small").height()*native_height - $(".large").height()/2)*-1;
+				var bgp = rx + "px " + ry + "px";
+				
+				//Time to move the magnifying glass with the mouse
+				var px = mx - $(".large").width()/2;
+				var py = my - $(".large").height()/2;
+				//Now the glass moves with the mouse
+				//The logic is to deduct half of the glass's width and height from the 
+				//mouse coordinates to place it with its center at the mouse coordinates
+                         $('.large').css({left:px,top:py,backgroundPosition:bgp});
+                     }
+                 }
+             });
+    
+     /*------magnifier------*/   
     
 });
 </script>
